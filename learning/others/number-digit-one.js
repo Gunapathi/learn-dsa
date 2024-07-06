dd0e855c8e3f53fb58760237505ac0efc36fea29
Gunapathi <51058500+Gunapathi@users.noreply.github.com>
2024-07-06 23:13:24 +0530

    updated comments

--- learning/others/number-digit-one.js
+++ learning/others/number-digit-one.js
@@ -45,26 +45,29 @@ Explanation 2:
 Digit 1 appears in 1(1 time) , 10(1 time) and 11(2 times) only. So the answer is 4. */
 
 function countDigitOne(A) {
-    let count = 0;
-    let factor = 1;
-    let n = A;
+    let count = 0; //  total count of digit '1'
+    let factor = 1; // current digit place (1 for units, 10 for tens, 100 for hundreds, etc.)
+    let n = A; // number of digits
 
-    while (n > 0) {
-        let digit = n % 10;
-        let higher = Math.floor(A / (factor * 10));
-        let lower = A % factor;
+    while (n > 0) { //loop runs as long as n is greater than 0.
+        let digit = n % 10; // current digit
+        let higher = Math.floor(A / (factor * 10)); // higher part of the number
+        let lower = A % factor; // lower part of the number  
 
         // Counting the number of ones contributed by the current digit place
         if (digit > 1) {
+            // If current_digit > 1, there are ((higher + 1) * factor) contributions of digit '1'.
             count += (higher + 1) * factor;
         } else if (digit === 1) {
+            // If the current_digit === 1, add the contributions from ((higher * factor) + (lower part + 1)) to include the current digit itself.
             count += higher * factor + lower + 1;
         } else {
+            // If the current_digit === 0, then count > (higher * factor).
             count += higher * factor;
         }
 
-        factor *= 10;
-        n = Math.floor(n / 10);
+        factor *= 10; // move to the next higher digit place
+        n = Math.floor(n / 10); // Update 'n', remove last digit from 'n'
     }
 
     return count;
