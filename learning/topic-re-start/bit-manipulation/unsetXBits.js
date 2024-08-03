6749568f7d91b7ccf83b95380cdddd276c042ee5
Gunapathi <51058500+Gunapathi@users.noreply.github.com>
2024-08-03 20:18:35 +0530

    bit manipulation 2

--- learning/topic-re-start/bit-manipulation/unsetXBits.js
+++ learning/topic-re-start/bit-manipulation/unsetXBits.js
@@ -5,7 +5,11 @@ For example, if A = 93 and B = 4, the binary representation of A is 1011101.
 If we unset the rightmost 4 bits, we get the binary number 1010000, which is equal to the decimal value 80. */
 
 function solve(A, B) {
-    return A & ~(1 << B)
+    A = BigInt(A);
+    // Create a mask with B rightmost bits as 0 and the rest as 1 using BigInt
+    let mask = ~((BigInt(1) << BigInt(B)) - BigInt(1));
+    // Perform bitwise AND operation to unset the B rightmost bits and return as BigInt
+    return Number(A & mask);
 }
 
 console.log(solve(25, 3)); // 24
